2c46185cdf8a01ef417c39c892acfd7c5f4871d5
elineviktoria <eline@tobiassen.no>
2025-08-08 12:49:41 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='containrt'>
+        <div class="text-container">
+          <h2 class="text">ehlma is under construction</h2>
+          <p class="text">{'{coming soon}'}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
