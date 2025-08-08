3d45ae9c21049c01eed9a2015e6e9804dbee859f
elineviktoria <eline@tobiassen.no>
2025-08-08 14:22:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <>
       <div className='containrt'>
         <div class="text-container">
-          <h1>Test</h1>
           <h2 class="text">ehlma is under construction</h2>
           <p class="text">{'{coming soon}'}</p>
         </div>
